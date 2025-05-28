b812b1434d222be019850ecfd07f3f463ef10cef
Adam Denis <90475646+AdamDenis@users.noreply.github.com>
2025-05-28 20:47:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $mysqli = new mysqli("db", "user", "password", "mydatabase");
 if ($mysqli->connect_error) {
     die("Conexiune eșuată: " . $mysqli->connect_error);
 }
-echo "Conexiune reușită la baza de date!";
+echo "Conexiune reușită la baza de date! ";
 ?>
